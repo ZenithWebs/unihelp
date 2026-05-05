2dce97717da3d2cd3c89fd5cfb3dbfacd3bdb963
ZenithWebs <iadejuwon77@gmail.com>
2026-05-05 13:27:58 +0100

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -8,30 +8,29 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
     VitePWA({
-      registerType: 'autoUpdate',
-       devOptions: {
-        enabled: true // ✅ prevents dev crash
+  registerType: 'autoUpdate',
+  strategies: 'generateSW',
+
+  manifest: {
+    name: 'UniHelp',
+    short_name: 'UniHelp',
+    start_url: '/',
+    display: 'standalone',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/favicon.png',
+        sizes: '192x192',
+        type: 'image/png'
       },
-      manifest: {
-        name: 'UniHelp',
-        short_name: 'UniHelp',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#000000',
-        icons: [
-          {
-            src: '/Favicon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/Favicon.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+      {
+        src: '/favicon.png',
+        sizes: '512x512',
+        type: 'image/png'
       }
-    }),
+    ]
+  }
+}),
     ],
   server: {
     headers: {
